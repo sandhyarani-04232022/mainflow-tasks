d0cf994d76005456578653175e49715db96d866c
Sandhyarani <104260354+sandhyarani-04232022@users.noreply.github.com>
2024-11-01 18:41:44 +0530

    Update main.js

--- internship_151024/js/main.js
+++ internship_151024/js/main.js
@@ -1,89 +1,26 @@
-activate_subtopnav_scroll = 0;
-from_scrollpos = -1;
-current_scrollpos = -1;
-goto_tut = 1;
-function startscrolling_subtopnav(event) {
-  event.preventDefault();
-  from_scrollpos = event.clientX;
-  activate_subtopnav_scroll = 1;
-}
-function scrolling_subtopnav(event) {  
-  current_scrollpos = event.clientX;
-  if (current_scrollpos == from_scrollpos) return false;
-  event.preventDefault();
-  if (event.buttons == 0) return false;
-  var scrollspeed;
-  if (activate_subtopnav_scroll == 1) {
-    goto_tut = 0;
-    scrollspeed = current_scrollpos - from_scrollpos;
-    scrollspeed = Math.abs(scrollspeed);
-    if (current_scrollpos < from_scrollpos) {
-     document.getElementById("scroll_left_btn").style.display = "block";     
-     document.getElementById("subtopnav").scrollLeft += scrollspeed;        
-    } else {
-      document.getElementById("subtopnav").scrollLeft -= scrollspeed;        
-    }
-    scrollbtn_visible();
-    from_scrollpos = current_scrollpos;
-  }
-}
-function endscrolling_subtopnav(event) {
-  event.preventDefault();
-  activate_subtopnav_scroll = 0;
-  from_scrollpos = -1;
-  current_scrollpos = -1;
-}
-function pellessii(event) {
-  if (goto_tut == 0) {
-    event.preventDefault();  
-    goto_tut = 1;
-    return false;
-  }
-}
+var button = document.getElementById('nav-right-btn');
+button.onclick = function () {
+    var subtopnav = document.getElementById('nav-library');
+    sideScroll(subtopnav,'right',25,100,10);
+};
 
-var scrollspeed = 1;
-var scrollinterval
-function scrollmenow(n) {
-  scrollinterval = window.setInterval(function() {
-    scrollspeed = scrollspeed * 1.1;
-    if (scrollspeed > 10) {scrollspeed = 10;}
-    if (n == 1) {
-      document.getElementById("subtopnav").scrollLeft += scrollspeed;  
-    } else {
-      document.getElementById("subtopnav").scrollLeft -= scrollspeed;  
-      
-    }
-    scrollbtn_visible();
-  }, 10);
+var back = document.getElementById('nav-left-btn');
+back.onclick = function () {
+    var subtopnav = document.getElementById('nav-librarya');
+    sideScroll(subtopnav,'left',25,100,10);
+};
+
+function sideScroll(element,direction,speed,distance,step){
+    scrollAmount = 0;
+    var slideTimer = setInterval(function(){
+        if(direction == 'left'){
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if(scrollAmount >= distance){
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
 }
-
-var p_s_l = 0;
-var x_s_l = 0;
-function scrollbtn_visible() {
-  var c_s_l = document.getElementById("subtopnav").scrollLeft;
-  if (c_s_l < 1) {
-    document.getElementById("scroll_left_btn").style.display = "none";    
-  } else {
-    document.getElementById("scroll_left_btn").style.display = "block";    
-  }
-  if (c_s_l > 1 && c_s_l == p_s_l) {
-    x_s_l++;
-  } else {
-    x_s_l = 0;  
-  }
-  if (x_s_l > 3) {
-    document.getElementById("scroll_right_btn").style.display = "none";    
-  } else {
-    document.getElementById("scroll_right_btn").style.display = "block";  
-  }
-  p_s_l = document.getElementById("subtopnav").scrollLeft;
-}
-
-function stopscrollmenow() {
-  scrollspeed = 1;
-  window.clearInterval(scrollinterval);
-}
-
-
-
-
